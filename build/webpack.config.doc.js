e35feb6e6332daa7a298d1225b7c6631c6fc4e3b
Bowen <z.bowen66@gmail.com>
2021-05-30 10:31:59 +0800

    build: fix docs

--- build/webpack.config.doc.js
+++ build/webpack.config.doc.js
@@ -8,7 +8,7 @@ module.exports = merge(baseConfig, {
   mode: 'production',
   entry: resolve('example/index.ts'),
   output: {
-    path: resolve('doc'),
+    path: resolve('docs'),
     publicPath: './'
   },
   plugins: [
