82b46d685468eed1e5bc155dc5827095f601c4b1
博文 <zhaobowen@mockuai.com>
2021-09-24 09:48:33 +0800

    style: remove comment

--- src/debounceAsyncResult/index.ts
+++ src/debounceAsyncResult/index.ts
@@ -1,12 +1,7 @@
 export default function debounceAsyncResult<T, P extends any[], R>(
   fn: (this: T, ...p: P) => Promise<R>,
-  // { normalDebounceInterval = 0 } = {},
 ) {
   let lastFetchId = 0;
-  // let run = debounce(() => {
-  //   fn.call(this, ...args)
-  // })
-
   return function asyncDebounced(this: T, ...args: P): Promise<R> {
     const fetchId = ++lastFetchId;
 
