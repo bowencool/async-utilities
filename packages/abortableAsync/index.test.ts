732ad60d294b02c88d3332cdca65d3f270ecb267
bowen <root@bowen.cool>
2022-03-24 16:07:05 +0800

    test(abortableAsync): add tests

--- packages/abortableAsync/index.test.ts
+++ packages/abortableAsync/index.test.ts
@@ -1,5 +1,57 @@
-describe('ddd', () => {
-  test('ss', async () => {
-    expect(Math.sqrt(4)).toBe(2);
+import { abortableAsync, AbortError, TimeoutError } from './index';
+import sinon from 'sinon';
+
+jest.useFakeTimers();
+
+afterEach(function () {
+  sinon.restore();
+});
+
+function someAsyncTask(delay = 1000, fail?: boolean): Promise<string> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (fail) {
+        reject('error');
+      } else {
+        resolve(`The result in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+describe('abortableAsync', () => {
+  test('basic', async () => {
+    const fn = abortableAsync(someAsyncTask);
+    const p = fn(1000);
+    // “快进”时间使得定时器回调被执行
+    jest.advanceTimersByTime(1000);
+    await expect(p).resolves.toBe('The result in 1000ms');
+
+    const p2 = fn(1000, true);
+    jest.advanceTimersByTime(1000);
+    await expect(p2).rejects.toBe('error');
+  });
+  test('timeout', async () => {
+    const fn = abortableAsync(someAsyncTask, { timeout: 2000 });
+    const p = fn(1000);
+    jest.advanceTimersByTime(1000);
+    await expect(p).resolves.toBe('The result in 1000ms');
+
+    const p2 = fn(2000);
+    jest.advanceTimersByTime(2000);
+    await expect(p2).rejects.toBeInstanceOf(TimeoutError);
+  });
+  test('signal', async () => {
+    // controller.abort() 只能触发一次 abort 事件
+    const controller = new AbortController();
+    const fn = abortableAsync(someAsyncTask, { signal: controller.signal });
+    const p = fn(1000);
+    jest.advanceTimersByTime(1000);
+    await expect(p).resolves.toBe('The result in 1000ms');
+
+    const p2 = fn(2000);
+    jest.advanceTimersByTime(1000);
+    controller.abort();
+    await expect(p2).rejects.toBeInstanceOf(AbortError);
   });
 });
