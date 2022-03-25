ad9b195f0d2a9c41fdcd1cdd302614cf9e19df29
bowencool <z.bowen66@gmail.com>
2022-03-25 03:47:23 +0000

    Deploying to gh-pages from @ bowencool/async-utilities@6f0ca5942837a3253d78608ce3893d4c78d9dde7 🚀

--- common.config.js
+++
@@ -1,8 +0,0 @@
-import pkg from '../package.json';
-
-export const replacement = {
-  'process.env.npm_package_version': JSON.stringify(process.env.npm_package_version || pkg.version),
-  // 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-};
-
-// export const alias
