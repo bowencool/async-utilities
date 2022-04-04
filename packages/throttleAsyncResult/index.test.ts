7760c8898878281f9a7ab0d6c852c8c793f86ff4
Bowen <root@bowen.cool>
2022-04-04 19:12:21 +0800

    test(throttleAsyncResult): add tests

---
+++ packages/throttleAsyncResult/index.test.ts
@@ -0,0 +1,71 @@
+import { throttleAsyncResult } from './index';
+
+// jest.useFakeTimers();
+function flushPromises() {
+  return new Promise((resolve) => setImmediate(resolve));
+}
+
+function someAsyncTask<T>(data: T, delay = 100, fail?: boolean): Promise<T> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (fail) {
+        reject(data);
+      } else {
+        resolve(data);
+      }
+    }, delay);
+  });
+}
+
+describe('throttleAsyncResult', () => {
+  test('keep result correctly', async () => {
+    jest.useFakeTimers();
+    const debounced = throttleAsyncResult(someAsyncTask);
+    const N = Math.random();
+    const p = debounced(N, 1);
+    jest.advanceTimersByTime(1000);
+    await expect(p).resolves.toBe(N);
+    const p2 = debounced(N, 0, true);
+    jest.advanceTimersByTime(1000);
+    await expect(p2).rejects.toBe(N);
+  });
+  test('keep result correctly with multiple calls', async () => {
+    jest.useFakeTimers();
+    const rawFn = jest.fn(someAsyncTask);
+    const debounced = throttleAsyncResult(rawFn);
+    const resolveOrReject = jest.fn();
+    new Array(5).fill(0).map((_, i) =>
+      debounced(i * 2, 100 - 10 * i, i % 2 === 0)
+        .then(resolveOrReject)
+        .catch(resolveOrReject),
+    );
+
+    expect(rawFn).toHaveBeenCalledTimes(1);
+    expect(resolveOrReject).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(500);
+    await flushPromises();
+    expect(rawFn).toHaveBeenCalledTimes(1);
+    expect(rawFn).lastCalledWith(0, 100, true);
+    expect(resolveOrReject).toHaveBeenCalledTimes(1);
+    expect(resolveOrReject).lastCalledWith(0);
+  });
+  test('useSamePromise', async () => {
+    jest.useFakeTimers();
+    const rawFn = jest.fn(someAsyncTask);
+    const debounced = throttleAsyncResult(rawFn, { useSamePromise: true });
+    const resolveOrReject = jest.fn();
+    new Array(5).fill(0).map((_, i) =>
+      debounced(i * 2, 100 - 10 * i, i % 2 === 0)
+        .then(resolveOrReject)
+        .catch(resolveOrReject),
+    );
+
+    expect(rawFn).toHaveBeenCalledTimes(1);
+    expect(resolveOrReject).not.toHaveBeenCalled();
+    jest.advanceTimersByTime(500);
+    await flushPromises();
+    expect(rawFn).toHaveBeenCalledTimes(1);
+    expect(rawFn).lastCalledWith(0, 100, true);
+    expect(resolveOrReject).toHaveBeenCalledTimes(5);
+  });
+});
